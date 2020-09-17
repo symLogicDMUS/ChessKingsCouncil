7ef8d38f9a1cfc52933710ea0df4e91370d3c8db
symLogicDMUS <brian.rathbone@wsu.edu>
2020-09-16 22:44:57 -0700

    first successfull call to firebase realtime db from react code

--- app/src/API/getDataDict.js
+++ app/src/API/getDataDict.js
@@ -1,5 +1,15 @@
-
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 export function getDataDict(username) {
-    var dataDict = db.
+    
+    return null;
+
+    // var dataDict = firebase.database().ref('game names').once('symLogicDMUS').then(function(snapshot) {
+    //     var gameNames = (snapshot.val())
+    //     return gameNames;
+    // })
+
+    // return dataDict;
 }
--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom";
-import {App} from "./App";
+import * as firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+// import {App} from "./App";
+//import {getDataDict} from "./API/getDataDict";
 
-ReactDOM.render(<App username="symLogicDMUS"/>, document.getElementById('root'))
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBSxIszSA63IMqXHfrzh29uVlrWUlBPf0k",
+    authDomain: "chess-king-council.firebaseapp.com",
+    databaseURL: "https://chess-king-council.firebaseio.com",
+    projectId: "chess-king-council",
+    storageBucket: "chess-king-council.appspot.com",
+    messagingSenderId: "154589429320",
+    appId: "1:154589429320:web:6216e373d165455f96a27d",
+    measurementId: "G-Y7XH9D79SR"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database().ref().child('game names').child('symLogicDMUS').once('value').then(function(snapshot) {
+    var gameNames = (snapshot.val())
+    ReactDOM.render(<div>{gameNames}</div>, document.getElementById('root'))
+})
+
+
+
+// ReactDOM.render(<App username="symLogicDMUS"/>, document.getElementById('root'))
